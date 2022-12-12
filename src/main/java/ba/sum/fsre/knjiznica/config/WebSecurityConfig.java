086e18a6a4c3c1f66708187e4081057e6f008c8e
Luka Lasic <luka.lasic@fsre.sum.ba>
2022-12-12 11:56:52 +0100

    Register radi

--- src/main/java/ba/sum/fsre/knjiznica/config/WebSecurityConfig.java
+++ src/main/java/ba/sum/fsre/knjiznica/config/WebSecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
@@ -31,5 +33,28 @@ public class WebSecurityConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/register_user/**","/","/register")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .usernameParameter("email")
+                .defaultSuccessUrl("/users")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+
+        http.authenticationProvider(authenticationProvider());
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
     }
 }
