6c604ff2a8019e3500bc3470998b2ef27124088e
Luka Lasić <luka.lasic@fsre.sum.ba>
2023-01-14 22:26:19 +0100

    9.1.2023.

--- src/main/java/ba/sum/fsre/knjiznica/model/User.java
+++ src/main/java/ba/sum/fsre/knjiznica/model/User.java
@@ -1,6 +1,8 @@
 package ba.sum.fsre.knjiznica.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
 
 @Entity
 @Table(name="users")
@@ -8,20 +10,43 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank(message = "Molimo unesite Vašu email adresu.")
+    @Email(message = "Unesite ispravnu email adresu.")
     @Column(unique = true, nullable = false, length = 50)
     private String email;
 
+    @NotBlank(message = "Molimo unesite Vašu lozinku.")
     @Column(nullable = false)
     private String password;
+    @NotBlank(message = "Molimo ponovite Vašu lozinku.")
+    private String passwordRepeat;
 
+    private boolean passwordsEqual;
+
+    public void setPasswordsEqual(boolean passwordsEqual) {
+        this.passwordsEqual = passwordsEqual;
+    }
+
+    public String getPasswordRepeat() {
+        return passwordRepeat;
+    }
+
+    public void setPasswordRepeat(String passwordRepeat) {
+        this.passwordRepeat = passwordRepeat;
+    }
+
+
+    @Size(min = 2, max = 30, message = "Vaše ime mora biti između 2 i 30 znakova duljine.")
     @Column(nullable = false, length = 30)
     private String firstname;
 
+
+    @Size(min = 2, max = 30, message = "Vaše prezime mora biti između 2 i 30 znakova duljine.")
     @Column(nullable = false, length = 30)
     private String lastname;
 
-    public User() {}
+    public User() {
+    }
 
     public User(Long id, String email, String password, String firstname, String lastname) {
         this.id = id;
@@ -70,5 +95,14 @@ public class User {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    @AssertTrue(message = "Lozinke se moraju podudarati.")
+    public boolean isPasswordsEqual() {
+        try {
+            return this.password.equals(this.passwordRepeat);
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 }
